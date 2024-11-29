8090a86f1ff6c0371e2dc67e819c5663fb39f741
Juliannn <161928537+wucebrayn3@users.noreply.github.com>
2024-11-29 22:58:35 +0800

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+let fnH1 = document.getElementById("fn");
+let lnH1 = document.getElementById("ln");
+
+let myFn = "SAMUEL";
+let myLn = "VALDEZ";
+let wc = "WELCOME";
+
+function delay(ms){
+    return new Promise(resolve=>setTimeout(resolve, ms));
+}
+
+async function showFn() {
+    await delay(1500);
+    for (let i = 0; i < myFn.length; i++){
+        fnH1.textContent += myFn[i]
+        await delay(50)
+    }
+}
+async function showLn() {
+    await delay(1700);
+    for (let i = 0; i < myLn.length; i++){
+        lnH1.textContent += myLn[i]
+        await delay(80)
+    }
+}
+
+async function cls(targ, basis, delaytime, start) {
+    await delay(start)
+    for (let i = 1; i <= myFn.length; i++){
+        targ.textContent = basis.slice(0, myFn.length - i)
+        await delay(delaytime)
+    }
+}
+
+async function welcome() {
+    await delay(5500)
+    for (let i = 0; i < wc.length; i++){
+        lnH1.textContent += wc[i];
+        await delay(50)
+    }
+}
+
+async function moveTooltip() {
+    
+}
+
+Promise.all([showFn(), showLn(), cls(fnH1, myFn, 40, 5000),cls(lnH1, myLn, 30, 4900), welcome()]);
+
+let navbar = document.getElementById("nav-bar");
+
+navbar.addEventListener("mouseenter", ()=>{
+    navbar.style.gap = "15%"
+})
+navbar.addEventListener("mouseleave", ()=>{
+    navbar.style.gap = ""
+})
+
+
+async function iThinkThisIsATooltip(input, basis){
+    wc = input;
+    if (lnH1.textContent == wc){
+        return
+    }
+
+    for (i = 0; i < input.length; i++){
+        lnH1.textContent = basis.slice(0, input.length - i - 1)
+        await delay(50)
+    }
+
+    await delay(250)
+
+    for (i = 0; i < input.length; i++){
+        lnH1.textContent += wc[i]
+        await delay(30)
+    }
+}
+
+async function cls2() {
+    
+}
+
+let navbarlinks = document.querySelectorAll(".links");
+
+navbarlinks.forEach(a=>{
+    a.addEventListener("mouseenter", ()=>{
+        a.querySelectorAll("p").forEach(b=>{
+            b.style.marginRight = "0.5em"
+            b.style.marginLeft = "0.5em"
+        })
+    iThinkThisIsATooltip(a.id, wc)
+    })
+    a.addEventListener("mouseleave", ()=>{
+        a.querySelectorAll("p").forEach(b=>{
+            b.style.marginRight = ""
+            b.style.marginLeft = ""
+        })
+    })
+})
